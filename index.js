6fc7057c95c8065d1efe045e4fddebcb009fa1f0
kushrm2803 <mahajankush2803@gmail.com>
2023-12-13 19:59:45 +0530

    created separate js file

---
+++ index.js
@@ -0,0 +1,37 @@
+localStorage.clear();
+
+function addToTable() {
+  let task_val = document.getElementById("task").value;
+  let description_val = document.getElementById("description").value;
+  let items_arr = [];
+  if (localStorage.getItem("items") == null) {
+    items_arr.push([task_val, description_val]);
+    localStorage.setItem("items", JSON.stringify(items_arr));
+  } else {
+    items_str = localStorage.getItem("items");
+    items_arr = JSON.parse(items_str);
+    items_arr.push([task_val, description_val]);
+    localStorage.setItem("items", JSON.stringify(items_arr));
+  }
+  let table_content = document.getElementById("table-contents");
+  let str = "";
+  items_arr.forEach((element, idx) => {
+    str += `<tr>
+      <th scope="row">${idx + 1}</th>
+      <td>${element[0]}</td>
+      <td>${element[1]}</td>
+      <td><button class="btn btn-sm btn-primary" onclick="deleteContent(${idx})">Delete</button></td>
+    </tr>`;
+  });
+  table_content.innerHTML = str;
+}
+
+function deleteContent(item_idx) {
+  let items_str1 = localStorage.getItem("items");
+  let items_arr1 = JSON.parse(items_str1);
+  items_arr1.splice(item_idx, 1);
+  localStorage.setItem("items", JSON.stringify(items_arr1));
+  addToTable();
+}
+addtolist = document.getElementById("add-to-list");
+addtolist.addEventListener("click", addToTable);
